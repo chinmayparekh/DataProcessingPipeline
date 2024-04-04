70cb4312dbef1450f996a74cdbafba9ce08c9897
Kritin P <pkritin@gmail.com>
2024-04-04 15:13:35 +0530

    Added multiple stages

--- lib/task_library.py
+++ lib/task_library.py
@@ -6,3 +6,13 @@ def display(input_file_path, output_file_path):
         with open(output_file_path, 'w') as output_file:
             output_file.write(content)
 
+
+def upper(input_file_path, output_file_path):
+    with open(input_file_path, 'r') as input_file:
+        content = input_file.read()
+        uppered = content.upper()
+        print(uppered)
+
+        with open(output_file_path, 'w') as output_file:
+            output_file.write(uppered)
+
