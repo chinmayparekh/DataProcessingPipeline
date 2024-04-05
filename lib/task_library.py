9e59931cc47884298dcb18e676742dcd40a1f73a
Sumanth <sumanth@Sumanths-MacBook-Air.local>
2024-04-05 16:28:10 +0530

    text functions implemented

--- lib/task_library.py
+++ lib/task_library.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.corpus import stopwords
+
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
 
 def display(input_file_path, output_file_path):
     with open(input_file_path, 'r') as input_file:
@@ -44,3 +52,69 @@ def mean(input_file_path, output_file_path, column_name):
     with open(output_file_path, 'w') as output_file:
         output_file.write(str(mean))
 
+
+
+def uppercase(input_file_path, output_file_path):
+    with open(input_file_path, 'r') as file:
+        text = file.read()
+    
+    text = text.upper()
+    
+    with open(output_file_path, 'w') as file:
+        file.write(text)
+
+# Function to convert text to lowercase
+def lowercase(input_file_path, output_file_path):
+    with open(input_file_path, 'r') as file:
+        text = file.read()
+    
+    text = text.lower()
+    
+    with open(output_file_path, 'w') as file:
+        file.write(text)
+
+# Function for tokenization
+def tokenize_text(input_file_path, output_file_path):
+    with open(input_file_path, 'r') as file:
+        text = file.read()
+    
+    tokens = word_tokenize(text)
+    
+    with open(output_file_path, 'w') as file:
+        file.write(' '.join(tokens))
+
+# Function for stemming
+def stem_text(input_file_path, output_file_path):
+    with open(input_file_path, 'r') as file:
+        text = file.read()
+    
+    porter = PorterStemmer()
+    tokens = word_tokenize(text)
+    stemmed_tokens = [porter.stem(token) for token in tokens]
+    
+    with open(output_file_path, 'w') as file:
+        file.write(' '.join(stemmed_tokens))
+
+# Function for lemmatization
+def lemmatize_text(input_file_path, output_file_path):
+    with open(input_file_path, 'r') as file:
+        text = file.read()
+    
+    lemmatizer = WordNetLemmatizer()
+    tokens = word_tokenize(text)
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    
+    with open(output_file_path, 'w') as file:
+        file.write(' '.join(lemmatized_tokens))
+
+# Function for stopword removal
+def remove_stopwords(input_file_path, output_file_path):
+    with open(input_file_path, 'r') as file:
+        text = file.read()
+    
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [token for token in tokens if token.lower() not in stop_words]
+    
+    with open(output_file_path, 'w') as file:
+        file.write(' '.join(filtered_tokens))
