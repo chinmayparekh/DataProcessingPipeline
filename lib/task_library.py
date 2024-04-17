5988472a3e1b46fd67b39157762a0c574f94a7db
Kritin P <pkritin@gmail.com>
2024-04-17 15:20:14 +0530

    feat: logging added

--- lib/task_library.py
+++ lib/task_library.py
@@ -5,6 +5,10 @@ from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.corpus import stopwords
 import mysql.connector
 import json
+import logging
+
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.INFO)
 
 
 nltk.download('punkt')
@@ -18,97 +22,122 @@ def get_file_path(database_name):
         for db_entry in json_data["databases"]:
             if db_entry["database"] == database_name:
                 return db_entry
-        return None
+        logging.error('Could not find database')
     
 def display(input_file_path, output_file_path):
     input_file_path = get_file_path(input_file_path)["path"]
     output_file_path = get_file_path(output_file_path)["path"]
     with open(input_file_path, 'r') as input_file:
         content = input_file.read()
-        print(content)
+        #print(content)
 
         with open(output_file_path, 'w') as output_file:
             output_file.write(content)
 
 def slice(input_file_path, output_file_path, column_name):
-    input_file_path = get_file_path(input_file_path)["path"]
-    output_file_path = get_file_path(output_file_path)["path"]
     try:
+        logging.info("Slicing DataFrame")
+        input_file_path = get_file_path(input_file_path)["path"]
+        output_file_path = get_file_path(output_file_path)["path"]
         df = pd.read_excel(input_file_path)
         sliced = df[column_name]
 
         with open(output_file_path, 'w') as output_file:
             output_file.write(str(sliced))
     except FileNotFoundError:
-        print("Error: Input file not found.")
+        logging.error("Input file not found.")
+        #print("Error: Input file not found.")
     except KeyError:
-        print("Error: Column not found in the DataFrame.")
+        logging.error("Column not found in the DataFrame.")
+        #print("Error: Column not found in the DataFrame.")
+    except TypeError as e:
+        logging.error(f"Type error: {e}")
+        #print(f"Error: Type error - {e}")
 
 # Function to dice a DataFrame by column names
 def dice(input_file_path, output_file_path, column_names):
-    input_file_path = get_file_path(input_file_path)["path"]
-    output_file_path = get_file_path(output_file_path)["path"]
-
     try:
-        print(column_names)
+        logging.info("Dicing DataFrame")
+        input_file_path = get_file_path(input_file_path)["path"]
+        output_file_path = get_file_path(output_file_path)["path"]
         df = pd.read_excel(input_file_path)
         diced = df[column_names]
 
         with open(output_file_path, 'w') as output_file:
             output_file.write(str(diced))
     except FileNotFoundError:
-        print("Error: Input file not found.")
+        logging.error("Input file not found.")
+        #print("Error: Input file not found.")
     except KeyError:
-        print("Error: Column not found in the DataFrame.")
+        logging.error("Column not found in the DataFrame.")
+        #print("Error: Column not found in the DataFrame.")
+    except TypeError as e:
+        logging.error(f"Type error: {e}")
+        #print(f"Error: Type error - {e}")
 
 # Function to remove null values from a DataFrame
 def remove_nulls(input_file_path, output_file_path):
-    input_file_path = get_file_path(input_file_path)["path"]
-    output_file_path = get_file_path(output_file_path)["path"]
     try:
+        logging.info("Removing null values from DataFrame.")
+        input_file_path = get_file_path(input_file_path)["path"]
+        output_file_path = get_file_path(output_file_path)["path"]
         df = pd.read_excel(input_file_path)
         no_nulls = df.dropna()
 
         with open(output_file_path, 'w') as output_file:
             output_file.write(str(no_nulls))
     except FileNotFoundError:
-        print("Error: Input file not found.")
-
+        logging.error("Input file not found.")
+        #print("Error: Input file not found.")
+    except TypeError as e:
+        logging.error(f"Type error: {e}")
+        #print(f"Error: Type error - {e}")
 # Function to convert a column to uppercase in a DataFrame
 def upper(input_file_path, output_file_path, column_name):
-    input_file_path = get_file_path(input_file_path)["path"]
-    output_file_path = get_file_path(output_file_path)["path"]
     try:
+        logging.info("Converting column to uppercase: ")
+        input_file_path = get_file_path(input_file_path)["path"]
+        output_file_path = get_file_path(output_file_path)["path"]
         df = pd.read_excel(input_file_path)
         df[column_name[0]] = df[column_name[0]].str.upper()
 
         with open(output_file_path, 'w') as output_file:
             output_file.write(str(df))
     except FileNotFoundError:
-        print("Error: Input file not found.")
+        logging.error("Input file not found.")
+        #print("Error: Input file not found.")
     except KeyError:
-        print("Error: Column not found in the DataFrame.")
-
+        logging.error("Column not found in the DataFrame.")
+        #print("Error: Column not found in the DataFrame.")
+    except TypeError as e:
+        logging.error(f"Type error: {e}")
+        #print(f"Error: Type error - {e}")
 # Function to calculate mean of a column in a DataFrame
 def mean(input_file_path, output_file_path, column_name):
-    input_file_path = get_file_path(input_file_path)["path"]
-    output_file_path = get_file_path(output_file_path)["path"]
     try:
+        logging.info("Calculating mean of column: ")
+        input_file_path = get_file_path(input_file_path)["path"]
+        output_file_path = get_file_path(output_file_path)["path"]
         df = pd.read_excel(input_file_path)
         mean_value = df[column_name].mean()
 
         with open(output_file_path, 'w') as output_file:
             output_file.write(str(mean_value))
     except FileNotFoundError:
-        print("Error: Input file not found.")
+        logging.error("Input file not found.")
+        #print("Error: Input file not found.")
     except KeyError:
-        print("Error: Column not found in the DataFrame.")
-
+        logging.error("Column not found in the DataFrame.")
+        #print("Error: Column not found in the DataFrame.")
+    except TypeError as e:
+        logging.error(f"Type error: {e}")
+        #print(f"Error: Type error - {e}")
 # Function to convert text to uppercase
 def uppercase(input_file_path, output_file_path):
-    input_file_path = get_file_path(input_file_path)["path"]
-    output_file_path = get_file_path(output_file_path)["path"]
     try:
+        logging.info("Converting text to uppercase.")
+        input_file_path = get_file_path(input_file_path)["path"]
+        output_file_path = get_file_path(output_file_path)["path"]
         with open(input_file_path, 'r') as file:
             text = file.read()
         
@@ -117,13 +146,17 @@ def uppercase(input_file_path, output_file_path):
         with open(output_file_path, 'w') as file:
             file.write(text)
     except FileNotFoundError:
-        print("Error: Input file not found.")
-
+        logging.error("Input file not found.")
+        #print("Error: Input file not found.")
+    except TypeError as e:
+        logging.error(f"Type error: {e}")
+        #print(f"Error: Type error - {e}")
 # Function to convert text to lowercase
 def lowercase(input_file_path, output_file_path):
-    input_file_path = get_file_path(input_file_path)["path"]
-    output_file_path = get_file_path(output_file_path)["path"]
     try:
+        logging.info("Converting text to lowercase.")
+        input_file_path = get_file_path(input_file_path)["path"]
+        output_file_path = get_file_path(output_file_path)["path"]
         with open(input_file_path, 'r') as file:
             text = file.read()
         
@@ -132,8 +165,11 @@ def lowercase(input_file_path, output_file_path):
         with open(output_file_path, 'w') as file:
             file.write(text)
     except FileNotFoundError:
-        print("Error: Input file not found.")
-
+        logging.error("Input file not found.")
+        #print("Error: Input file not found.")
+    except TypeError as e:
+        logging.error(f"Type error: {e}")
+        #print(f"Error: Type error - {e}")
 # Function for tokenization
 def tokenize_text(input_file_path, output_file_path):
     input_file_path = get_file_path(input_file_path)["path"]
@@ -146,9 +182,13 @@ def tokenize_text(input_file_path, output_file_path):
         
         with open(output_file_path, 'w') as file:
             file.write(' '.join(tokens))
+        
+        logging.info("Tokenization completed successfully for file: %s", input_file_path)
     except FileNotFoundError:
-        print("Error: Input file not found.")
-
+        logging.error("Input file not found: %s", input_file_path)
+    except TypeError as e:
+        logging.error(f"Type error: {e}")
+        #print(f"Error: Type error - {e}")
 # Function for stemming
 def stem_text(input_file_path, output_file_path):
     input_file_path = get_file_path(input_file_path)["path"]
@@ -163,9 +203,13 @@ def stem_text(input_file_path, output_file_path):
         
         with open(output_file_path, 'w') as file:
             file.write(' '.join(stemmed_tokens))
+        
+        logging.info("Stemming completed successfully for file: %s", input_file_path)
     except FileNotFoundError:
-        print("Error: Input file not found.")
-
+        logging.error("Input file not found: %s", input_file_path)
+    except TypeError as e:
+        logging.error(f"Type error: {e}")
+        #print(f"Error: Type error - {e}")
 # Function for lemmatization
 def lemmatize_text(input_file_path, output_file_path):
     input_file_path = get_file_path(input_file_path)["path"]
@@ -180,9 +224,13 @@ def lemmatize_text(input_file_path, output_file_path):
         
         with open(output_file_path, 'w') as file:
             file.write(' '.join(lemmatized_tokens))
+        
+        logging.info("Lemmatization completed successfully for file: %s", input_file_path)
     except FileNotFoundError:
-        print("Error: Input file not found.")
-
+        logging.error("Input file not found: %s", input_file_path)
+    except TypeError as e:
+        logging.error(f"Type error: {e}")
+        #print(f"Error: Type error - {e}")
 # Function for stopword removal
 def remove_stopwords(input_file_path, output_file_path):
     input_file_path = get_file_path(input_file_path)["path"]
@@ -197,32 +245,24 @@ def remove_stopwords(input_file_path, output_file_path):
         
         with open(output_file_path, 'w') as file:
             file.write(' '.join(filtered_tokens))
+        
+        logging.info("Stopword removal completed successfully for file: %s", input_file_path)
     except FileNotFoundError:
-        print("Error: Input file not found.")
+        logging.error("Input file not found: %s", input_file_path)
 
-
-def sql_connection(input_file_path, output_file_path):
-    with open('config/config.json') as config_file:
-        config = json.load(config_file)
-
-    mydb = mysql.connector.connect(
-        host = config['server'],
-        user = config['user'],
-        password = config['password'],
-        database = config['database']
-    )
-    print(mydb)
-    return mydb
-
-import json
-import mysql.connector
+    except TypeError as e:
+        logging.error(f"Type error: {e}")
+        #print(f"Error: Type error - {e}")
 
 def sql_query(input_file_path, output_file_path, query):
-    try:
-        print("query: " + str(query[0]))
+    output_file_path = get_file_path(output_file_path)["path"]
+    config = get_file_path(input_file_path)
 
-        with open('config/config.json') as config_file:
-            config = json.load(config_file)
+    #print(input_file_path)
+    try:
+        #print("query: " + str(query[0]))
+
+
         
         mydb = mysql.connector.connect(
             host = config['server'],
@@ -233,20 +273,25 @@ def sql_query(input_file_path, output_file_path, query):
         cursor = mydb.cursor()
 
         cursor.execute(str(query[0]))
-
+        logging.info("query: " + str(query))
         rows = cursor.fetchall()
 
         with open(output_file_path, 'w') as output_file:
             for row in rows:
                 output_file.write(str(row) + '\n')
-                print(row)
+                #print(row)
 
         cursor.close()
         mydb.close()
 
     except FileNotFoundError:
-        print("Error: Config file not found.")
+        logging.error("Config file not found")
     except mysql.connector.Error as db_error:
-        print("MySQL Error:", db_error)
+        logging.error("MySQL Error: %s", db_error)
     except Exception as e:
-        print("An error occurred:", str(e))
+        logging.error("An error occurred: %s", str(e))
+    except TypeError as e:
+        logging.error(f"Type error: {e}")
+        #print(f"Error: Type error - {e}")
+
+
