8c85146808f9baa1410b850ab6878f6ee1da1f0f
Kritin P <pkritin@gmail.com>
2024-05-04 01:43:40 +0530

    minor change

--- demo/use_cases/text/test.py
+++ demo/use_cases/text/test.py
@@ -1,7 +1,7 @@
 from dpp import xml_parser as xml_parser
 
 if __name__ == '__main__':
-    if xml_parser.validate_xml('/home/chinmay/chin/semester8/data_modelling/project/data_processing_pipeline/DataProcessingPipeline/demo/use_cases/text/input_text.xml'):
-        xml_parser.parse_and_execute("/home/chinmay/chin/semester8/data_modelling/project/data_processing_pipeline/DataProcessingPipeline/demo/use_cases/text/input_text.xml")
+    if xml_parser.validate_xml('/home/kritin/College/DM/Project/DataProcessingPipeline/demo/use_cases/text/input_text.xml'):
+        xml_parser.parse_and_execute("/home/kritin/College/DM/Project/DataProcessingPipeline/demo/use_cases/text/input_text.xml")
     else:
         print("The XML file is invalid.")
