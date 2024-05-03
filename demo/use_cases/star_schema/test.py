831141c8c6133c01fa768a881ce8d2342cdd2db8
“Your <you@example.com>
2024-05-03 23:24:52 +0530

    fixed all paths and udf bugs

--- demo/use_cases/star_schema/test.py
+++ demo/use_cases/star_schema/test.py
@@ -1,7 +1,7 @@
 from dpp import xml_parser as xml_parser
 
 if __name__ == '__main__':
-    if xml_parser.validate_xml('/chin/semester8/data_modelling/project/data_processing_pipeline/demo/use_cases/star_schema/test_join.xml'):
-        xml_parser.parse_and_execute("/chin/semester8/data_modelling/project/data_processing_pipeline/demo/use_cases/star_schema/test_join.xml")
+    if xml_parser.validate_xml('/home/chinmay/chin/semester8/data_modelling/project/data_processing_pipeline/DataProcessingPipeline/demo/use_cases/star_schema/test_join.xml'):
+        xml_parser.parse_and_execute("/home/chinmay/chin/semester8/data_modelling/project/data_processing_pipeline/DataProcessingPipeline/demo/use_cases/star_schema/test_join.xml")
     else:
         print("The XML file is invalid.")
